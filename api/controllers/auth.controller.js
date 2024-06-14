c2b2663c2837015eaa1bebaeebe677eff01ded46
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-06-15 00:16:44 +0530

    update auth controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,7 +19,7 @@ const signup = async (req, res) => {
     // send response
     res.status(201).json("User created successfully!");
   } catch (error) {
-    res.status(500).json(error.message);
+   next(error);
   }
 };
 
--- api/index.js
+++ api/index.js
@@ -29,3 +29,17 @@ app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 
 
+// middlewares 
+app.use((err, req, res, next) => {
+   const statusCode = err.statusCode || 500;
+   const message = err.message || 'Internal Server Error';
+
+   return res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message
+   })
+
+
+})
+
