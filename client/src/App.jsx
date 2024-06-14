03b768238e6d5cf07127ef43a50b0893cc02e608
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-06-14 16:57:03 +0530

    add the header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import SignIn from "./pages/SignIn.jsx"
 import SignUp from "./pages/SignUp.jsx"
 import About from "./pages/About.jsx"
 import Profile from "./pages/Profile.jsx"
+import Header from "./components/Header.jsx"
 
 
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,48 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md ">
+      <div className="flex items-center justify-between max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
+            <span className="text-slate-500">Bharat</span>
+            <span className=" text-slate-700">Estate</span>
+          </h1>
+        </Link>
+
+        <form className="bg-slate-100 p-3 rounded-lg flex  items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-500" />
+        </form>
+
+        <ul className="flex items-center gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer ">
+              Home
+            </li>
+          </Link>
+
+          <Link to="/about" >
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer ">
+              About
+            </li>
+          </Link>
+
+          <Link to="/sign-in" >
+            <li className="sm:inline text-slate-700 hover:underline cursor-pointer">
+              Sign in
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
