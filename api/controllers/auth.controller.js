75752889d675cd8143621329ec568d9cf4908da5
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-06-14 23:53:12 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // hashed user password by bcryptjs which will save in database
+  // const hashedPassword = await bcryptjs.hash(password,10);
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  // create a new user
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  // user validation check
+  try {
+    // then save newUser in mongodb database
+    await newUser.save();
+    // send response
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+export { signup };
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config();
 
@@ -20,6 +21,11 @@ app.listen(3000, () => {
 });
 
 
+// send data as a json form 
+app.use(express.json())
+
+// routes
 app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
 
 
