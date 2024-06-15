85f9033bce090eccdaf271e7c7dfdb1f955e467c
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-06-15 20:33:31 +0530

    add redux  persist

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"; 
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
