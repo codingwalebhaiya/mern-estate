e016a3253ca3d94fb526830cad372959d812dadc
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-06-15 10:47:03 +0530

    create a sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,47 @@
-
+import {Link} from "react-router-dom"
 
 function SignUp() {
   return (
-    <div>
-      sign up page 
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4 ">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg "
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg "
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg "
+          id="password"
+        />
+
+        <button
+          className="bg-slate-700 text-white p-3 
+        rounded-lg uppercase hover:bg-opacity-95 disabled:opacity-80 "
+        >
+          Sign Up
+        </button>
+      </form>
+
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-700 hover:underline">
+            Sign in
+          </span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
