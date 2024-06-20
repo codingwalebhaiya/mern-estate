ac11a091043a5036bb37aae0bc93d6a0b7a35520
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-06-20 11:53:27 +0530

    add user delete functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,17 +1,19 @@
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js";
- 
+
 export const test = (req, res) => {
   res.json({
     message: "jay jay shree ram - So api route is working successfully",
   });
-}; 
+};
 
+
+// update the user 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id) {
     return next(errorHandler(401, "You can only update your own account"));
-  }
+  } 
   try {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -35,6 +37,21 @@ export const updateUser = async (req, res, next) => {
     res.status(200).json({
       rest,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+// delete the user
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(402, "You can only delete your own account"));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted successfully!");
   } catch (error) {
     next(error);
   }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
 import verifyUser from "../utils/verifyUser.js";
 
 
@@ -11,6 +11,7 @@ const router = express.Router()
 
 router.get('/test', test)
 router.post('/update/:id',verifyUser, updateUser)
-
+router.delete('/delete/:id',verifyUser, deleteUser)
+ 
 export default router;
  
