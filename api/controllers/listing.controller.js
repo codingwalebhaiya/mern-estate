5da0b7183d6596f7aefcf110c8542812a055fcba
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-06-20 21:41:05 +0530

    create listing api route

---
+++ api/controllers/listing.controller.js
@@ -0,0 +1,13 @@
+import Listing from "../models/listing.model.js"
+
+export const createListing  = async(req,res,next)  => {
+
+    try {
+        const listing = await Listing.create(req.body);
+        return res.status(201).json({ message: "Listing created successfully!", listing })
+    } catch (error) {
+        next(error);    
+    }
+}
+
+  
