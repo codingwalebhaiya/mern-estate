fae94d129334dfd380774b36b785001a7ebe7b6f
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-07-09 18:38:17 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,14 @@
 
 function About() {
   return (
-    <div>
-      about page 
+    <div className="flex flex-col py-20 px-4 max-w-6xl mx-auto items-start  gap-3">
+     <h1 className="text-3xl font-bold font-serif text-slate-800">About Bharat Estate</h1>
+     <div className=" flex flex-col gap-2 ">
+      <p className="text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa excepturi voluptates culpa minima harum quis fugiat inventore fuga dolorum animi.</p>
+    <p className="text-slate-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam illum amet voluptatum maxime? Harum, corporis!</p>
+    <p className="text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore aperiam neque itaque quas accusamus quibusdam dolor, ex illum illo, temporibus natus molestiae!</p>
+     </div>
+   
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -52,15 +52,14 @@ function Home() {
     <div>
       {/* top */}
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
-        <h1 className="text-slate-700 font-bold text-3xl">
-          Find your next <span className="text-slate-500">perfect</span>
-          <br /> place with ease
+        <h1 className=" text-3xl text-slate-700 font-bold ">
+        Welcome to Bharat Estate your <span className="text-slate-500">perfect</span>
+          <br /> Place Awaits!
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm ">
-          Bharat Estate is the best place to find your next perfect place to
-          live.
+        Explore a World of Properties with Bharat Estate.
           <br />
-          We have a wide range of properties for you to choose from.
+          Find Your Perfect Match Today
         </div>
 
         <Link
