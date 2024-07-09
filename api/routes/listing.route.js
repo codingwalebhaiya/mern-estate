b0a91bfe3432bdd72d09aa4a2af0b2583514ebaf
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-07-09 14:47:08 +0530

    update the search page, create a listing item component & show listings

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,7 +2,7 @@ import express from "express"
 import verifyToken from "../utils/verifyUser.js"
 import { createListing, deleteListing,getListing,updateListing, getListings } from "../controllers/listing.controller.js"
 
-const router = express.Router()
+const router = express.Router() 
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing );
