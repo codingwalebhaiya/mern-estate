eacb86436bb6523ddfb0de5f25e270ce4943b69e
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-07-21 10:14:52 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import path from "path";
 
 
 
@@ -20,6 +20,10 @@ mongoose
     console.log(err); 
   });
 
+  //dynamic directory name 
+  const __dirname = path.resolve();
+
+
 const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000 !!");
@@ -31,13 +35,17 @@ app.use(express.json());
 // get the data from cookies
 app.use(cookieParser());
 
-app.use(cors());
-
 
 // routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 // middlewares
 app.use((err, req, res, next) => {
